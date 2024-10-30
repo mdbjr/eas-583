eb5c63103833097ca00801b3ed394d32f62a5ea9
MARC\marcb <marc.baker.jr@gmail.com>
2024-10-30 17:51:30 -0400

    no message

--- verify.py
+++ verify.py
@@ -6,6 +6,9 @@ from web3.middleware import geth_poa_middleware
 import json
 import hashlib
 
+import connect_to_eth
+
+
 def signChallenge( challenge ):
 
     w3 = Web3()
@@ -26,6 +29,7 @@ def verifySig():
         We've added it here for testing 
     """
 
+
     challenge_bytes = random.randbytes(32)
 
     challenge = encode_defunct(challenge_bytes)
@@ -39,6 +43,20 @@ if __name__ == '__main__':
     """
         Test your function
     """
+    #w3 = connect_to_eth.connect_to_eth()
+    w3 = Web3(Web3.HTTPProvider("https://api.avax-test.network/ext/bc/C/rpc"))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    contract_address = "0x85ac2e065d4526FBeE6a2253389669a12318A412"
+
+
+    with open('NFT_abi.json', 'r') as f:
+        contract_abi = json.load(f)
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+    sk = '7909e1cfd3d865eccdbcd9676804f16bedebba61963d7c44406ecb53e2653787'#"YOUR SECRET KEY HERE"
+
+    acct = w3.eth.account.from_key(sk)
+    i=55
+    contract.functions.claim(acct.address, i.to_bytes(32, byteorder='big')).call()
     if verifySig():
         print( f"You passed the challenge!" )
     else:
