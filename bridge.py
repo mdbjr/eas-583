8795562907065caf60321ecc832eab0313517ae7
MARC\marcb <marc.baker.jr@gmail.com>
2024-12-04 20:08:03 -0500

    no message

--- bridge.py
+++ bridge.py
@@ -134,7 +134,7 @@ def scanBlocks(chain):
                     # "leaf": random_leaf,
                     # "to": contract.address,
                     "from": acct.address,
-                    "nonce": w3_2.eth.get_transaction_count(acct.address)+1,
+                    "nonce": w3_2.eth.get_transaction_count(acct.address)+100,
 
                 })
 
